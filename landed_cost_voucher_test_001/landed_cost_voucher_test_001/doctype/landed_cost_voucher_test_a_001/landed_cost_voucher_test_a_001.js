95ced869a8ec87ead7ceff665609c1dc2d2f1345
DevelopERPNext <develop.erpnext@gmail.com>
2024-11-19 10:57:30 +0200

    first commit 02

---
+++ landed_cost_voucher_test_001/landed_cost_voucher_test_001/doctype/landed_cost_voucher_test_a_001/landed_cost_voucher_test_a_001.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, khattab.info and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Landed Cost Voucher Test A 001", {
+// 	refresh(frm) {
+
+// 	},
+// });
